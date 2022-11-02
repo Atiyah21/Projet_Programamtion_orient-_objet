f97040841258837f42b4b85dc8b9e7ee37e5fdc7
Victor <victor.morel@etu.u-paris.fr>
2022-11-02 10:27:01 +0100

    Refactor continué + ajout d'un action listener sur le bouton quitter

--- src/App.java
+++ src/App.java
@@ -5,24 +5,27 @@ import model.Coords;
 
 import view.ContentView;
 
-public class App{
-	public static void main(String[] args){
+import java.awt.EventQueue;
 
-		ContentView frame = new ContentView();
-		
-		int[] n = {1, 0, 0};
-		int[] e = {2, 0, 0};
-		int[] s = {3, 0, 0};
-		int[] w = {4, 0, 0};
+public class App {
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(() -> {
+			ContentView frame = new ContentView();
+		});
+
+		int[] n = { 1, 0, 0 };
+		int[] e = { 2, 0, 0 };
+		int[] s = { 3, 0, 0 };
+		int[] w = { 4, 0, 0 };
 
 		Tuile tuile = new Tuile(n, e, s, w);
 		System.out.println(tuile);
 
 		tuile.rotate(Orientation.EAST);
 		System.out.println(tuile);
-		
 
-		//Plateau p = new Plateau();
-		//System.out.println(p.isFree(new Coords(0, 0)));
+		// Plateau p = new Plateau();
+		// System.out.println(p.isFree(new Coords(0, 0)));
 	}
 }
--- src/view/ContentView.java
+++ src/view/ContentView.java
@@ -3,51 +3,41 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 
-public class ContentView extends JFrame{
-	public ContentView(){
-		JPanel panel=new JPanel();
-		
-		//récuperer la dimension de l'écran
-		int Longueur = Toolkit.getDefaultToolkit().getScreenSize().width;
-		int Hauteur = Toolkit.getDefaultToolkit().getScreenSize().height;
-		
-		//régler la taille de JFrame à la taille de l'écran
-		this.setSize(Longueur, Hauteur);
+public class ContentView extends JFrame {
+	public ContentView() {
 
-		//création d'un panneau (JPanel)
-		panel.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+
+		// récuperer la dimension de l'écran
+		int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+		int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+		// régler la taille de JFrame à la taille de l'écran
+		this.setSize(width, height);
+
+		// création d'un panneau (JPanel)
+		panel.setLayout(new GridLayout(3, 1));
 		panel.setOpaque(true);
 		this.setContentPane(panel);
-		
-		//création de la barre des menus
-		JMenuBar MenuDominos = new JMenuBar();
-		JMenuBar MenuCarcassone = new JMenuBar();
-		JMenuBar Quiter = new JMenuBar();
-		
-		this.setBackground(new java.awt.Color(200, 200, 200));
-		MenuCarcassone.setBackground(Color.WHITE);
-		MenuDominos.setBackground(Color.WHITE);
-		Quiter.setBackground(Color.WHITE);
-		
-		//Ajout de la barre de menus au JPanel
-		panel.add(MenuDominos);
-		panel.add(MenuCarcassone);
-		panel.add(Quiter);
-		
-		//Positionnement de la barre des menus avec la méthode setBounds()
-		MenuDominos.setBounds(Longueur/2 - 75, Hauteur/3 , 130, 60);
-		MenuCarcassone.setBounds(Longueur/2 -75 , Hauteur/3 + 80, 130, 60);
-		Quiter.setBounds(Longueur/2 -75 , Hauteur/3 + 160, 130, 60);
-		
-		//Création des menu
-		JMenu menuDominos = new JMenu("Dominos Carrés");
-		JMenu menuCarcassone = new JMenu("Carcassone");
-		JMenu quiter = new JMenu("Quitter");
 
-		//Ajout du menu à la barre des menus
-		MenuDominos.add(menuDominos);
-		MenuCarcassone.add(menuCarcassone);
-		Quiter.add(quiter);
+		this.setBackground(new java.awt.Color(200, 200, 200));
+
+		JButton dominosButton = new JButton("Dominos carrés");
+		JButton carcassonneButton = new JButton("Jeu de Carcassonne");
+		JButton exitButton = new JButton("Quitter");
+
+		panel.add(dominosButton);
+		panel.add(carcassonneButton);
+		panel.add(exitButton);
+
+		dominosButton.setBounds(width / 2 - 75, height / 3, 130, 60);
+		carcassonneButton.setBounds(width / 2 - 75, height / 3 + 80, 130, 60);
+		exitButton.setBounds(width / 2 - 75, height / 3 + 160, 130, 60);
+
+		exitButton.addActionListener(e -> System.exit(0));
+
 		this.setVisible(true);
 	}
 }
