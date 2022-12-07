aba4e6ed841f7910334eafac067fb76600e8c664
Ayman <ayman.khemakhem@etu.u-paris.fr>
2022-12-07 17:29:46 +0100

    Amelioration

--- src/view/dominos/DominosView.java
+++ src/view/dominos/DominosView.java
@@ -5,6 +5,9 @@ import java.awt.*;
 
 public class DominosView extends JFrame {
     public DominosView() {
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+		int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+		this.setSize(width, height);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         TuileView t = new TuileView();
