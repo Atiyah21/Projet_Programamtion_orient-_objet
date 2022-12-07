6ff5d7853b5e9093b9a03dbd87bd46476ed000d2
Ayman <ayman.khemakhem@etu.u-paris.fr>
2022-12-07 17:58:39 +0100

    Tuiles mise en bas et organisation de la fenetre du domino

--- src/view/dominos/DominosView.java
+++ src/view/dominos/DominosView.java
@@ -1,19 +1,32 @@
 package view.dominos;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
 public class DominosView extends JFrame {
     public DominosView() {
         int width = Toolkit.getDefaultToolkit().getScreenSize().width;
 		int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+
 		this.setSize(width, height);
+        this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new FlowLayout());
+        
         TuileView t = new TuileView();
         TuileView t2 = new TuileView();
-        this.add(t);
-        this.add(t2);
+        
+        JPanel pioche = new JPanel();
+        pioche.setLayout(new FlowLayout());
+        pioche.add(t);
+        pioche.add(t2);
+
+        this.add(new JLabel(), BorderLayout.CENTER);
+        this.add(new JLabel(), BorderLayout.NORTH);
+        this.add(new JLabel(), BorderLayout.EAST);
+        this.add(new JLabel(), BorderLayout.WEST);
+        this.add(pioche, BorderLayout.SOUTH);
         this.setVisible(true);
     }
 }
