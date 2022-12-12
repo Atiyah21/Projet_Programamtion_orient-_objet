a1aba1e83bd5e29b4cff367411a45a2c57cc82af
Victor <victor.morel@etu.u-paris.fr>
2022-12-12 14:32:27 +0100

    Bug des hasmap corrigé

--- src/model/Coords.java
+++ src/model/Coords.java
@@ -1,20 +1,27 @@
 package model;
 
-public class Coords{
+public class Coords {
 	int x;
 	int y;
 
-	public Coords(int x, int y){
+	public Coords(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	public boolean equals(Coords other){
-		return x == other.x && y == other.y;
+	public boolean equals(Object other) {
+		if (!(other instanceof Coords))
+			return false;
+		Coords c = (Coords) other;
+		return x == c.x && y == c.y;
 	}
 
-	public int hashCode(){
-		int tmp = (y+((x+1)/2));
+	public int hashCode() {
+		int tmp = (y + ((x + 1) / 2));
 		return x + (tmp * tmp);
+	}
+
+	public String toString() {
+		return "x: " + x + " y:" + y;
 	}
 }
