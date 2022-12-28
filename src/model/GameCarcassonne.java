8a6f23d4e0191e68426b5d494ce3ea8096ebdc4f
Ayman <ayman.khemakhem@etu.u-paris.fr>
2022-12-28 18:57:39 +0100

    sac aleatoire et ajout des pieces qui sont en plusieurs fois

--- src/model/GameCarcassonne.java
+++ src/model/GameCarcassonne.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 import model.SideCarcassonne.Sike;
@@ -12,31 +13,41 @@ public class GameCarcassonne extends Game{
         
         sac = new Stack<>();
         sac.push(new TuileCarcassone(Sike.CHEMIN, Sike.CHEMIN, Sike.CHEMIN, Sike.CHEMIN, false));
-        sac.push(new TuileCarcassone(Sike.CHEMIN, Sike.PRE, Sike.CHEMIN, Sike.PRE, false));
-        sac.push(new TuileCarcassone(Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, Sike.CHEMIN, false));
-        sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, false));
-        sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.CHEMIN, Sike.PRE, false));
-        sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.PRE, Sike.PRE, false));
-        sac.push(new TuileCarcassone(Sike.PRE, Sike.VILLE, Sike.PRE, Sike.VILLE, true));
-        sac.push(new TuileCarcassone(Sike.PRE, Sike.VILLE, Sike.PRE, Sike.VILLE, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.CHEMIN, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.PRE, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.VILLE, true));
-        
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.VILLE, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.PRE, Sike.CHEMIN, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, false));
+        for(int i=0; i<3; i++){
+            sac.push(new TuileCarcassone(Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, Sike.CHEMIN, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.PRE, Sike.PRE, false));
+            sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.PRE, Sike.PRE, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.PRE, Sike.CHEMIN, false));
+        }
         sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.PRE, Sike.PRE, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.PRE, Sike.VILLE, true));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.PRE, Sike.VILLE, false));
+        sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, false));
+        for(int i=0; i<8; i++){
+            sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, false));
+            sac.push(new TuileCarcassone(Sike.CHEMIN, Sike.PRE, Sike.CHEMIN, Sike.PRE, false));
+        }
+        for(int i=0; i<3; i++){
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.PRE, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.VILLE, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.CHEMIN, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.PRE, Sike.VILLE, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.PRE, Sike.VILLE, false));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.CHEMIN, Sike.CHEMIN, false));
+            sac.push(new TuileCarcassone(Sike.PRE, Sike.VILLE, Sike.PRE, Sike.VILLE, false));
+        }
+        for(int i=0; i<2; i++){
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.CHEMIN, Sike.CHEMIN, Sike.VILLE, true));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.PRE, Sike.PRE, false));
+            sac.push(new TuileCarcassone(Sike.PRE, Sike.PRE, Sike.CHEMIN, Sike.PRE, false));
+            sac.push(new TuileCarcassone(Sike.PRE, Sike.VILLE, Sike.PRE, Sike.VILLE, true));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.CHEMIN, Sike.VILLE, true));
+            sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.PRE, Sike.VILLE, true));
+        }
         sac.push(new TuileCarcassone(Sike.VILLE, Sike.PRE, Sike.VILLE, Sike.PRE, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.CHEMIN, Sike.VILLE, true));
         sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.CHEMIN, Sike.VILLE, false));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.PRE, Sike.PRE, false));
         sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.PRE, Sike.VILLE, true));
-        sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.PRE, Sike.VILLE, false));
         sac.push(new TuileCarcassone(Sike.VILLE, Sike.VILLE, Sike.VILLE, Sike.VILLE, true));
 
+        Collections.shuffle(sac);
         players = new ArrayList<>();
 		players.add(new Player("Victor"));
         plateau = new Plateau(sac.pop());
